3e4746cf810e7cd493fe705e5427defbd2e38fe3
Saul <saulc@users.noreply.github.com>
2019-09-23 22:23:12 -0700

    One way com?
    feels like something...

--- SimpleSocket.py
+++ SimpleSocket.py
@@ -7,26 +7,30 @@ import socket
 class ssocket:
     def __init__(self, blah):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connected = False
+        self.msg = ""
 
     def connect(self, host, port):
         self.sock.connect((host, port))
+        self.connected = True
 
     def setupServer(self, host, port):
         self.sock.bind((host, port))
         self.sock.listen()
-        conn, addr = self.sock.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
-                print('Message Received: ' + str(data))
+        self.connected = True
+        self.mreceive()
 
 
     def msend(self, msg):
-        pass
+        self.sock.sendall(str.encode(msg))
 
     def mreceive(self):
-        pass
+            conn, addr = self.sock.accept()
+            with conn:
+                print('Connected by', addr)
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    conn.sendall(data)
+                    print('Message Received: ' + str(data))
--- tktest.py
+++ tktest.py
@@ -6,7 +6,7 @@ from SimpleSocket import ssocket
 
 class Application(tk.Frame):
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master, background ='black')
+        tk.Frame.__init__(self, master, background ='blue')
         self.pack()
         self.createWidgets()
         self.sock = ssocket(None)
@@ -15,7 +15,7 @@ class Application(tk.Frame):
 #setup ui elements/layout
     def createWidgets(self):
         self.connect = tk.Button(self)
-        # self.connect.configure(background='black')
+        self.connect.configure(background='black')
         self.connect["text"] = "Connect"
         self.connect["command"] = self.hookup
         self.connect.pack( fill = tk.X)
@@ -23,17 +23,24 @@ class Application(tk.Frame):
         self.pbar =  ttk.Progressbar(self, orient="horizontal", length=260, mode="determinate")
         self.pbar.pack( fill = tk.X)
 
+        self.cc = tk.Button(self, text="connect to server", command=self.cconn)
+        self.cc.pack( fill = tk.X)
+
         self.option = tk.Button(self)
-        # self.option.configure(background='black')
+        self.option.configure(background='black')
         self.option["text"] = "Options"
         self.option["command"] = self.say_hi
         self.option.pack( fill = tk.X)
 
-        self.QUIT = tk.Button(self, text="QUIT", bg='black' ,fg="red", command=root.destroy)
+        self.QUIT = tk.Button(self, text="QUIT", command=root.destroy)
         self.QUIT.pack( fill = tk.X)
 
     def getPort(self):
         return 65432
+
+    def cconn(self):
+        host = "127.0.0.1"      #make this real later...
+        self.sock.connect(host, self.getPort())
 
         # connect
     def hookup(self):
@@ -44,12 +51,16 @@ class Application(tk.Frame):
         self.sock.setupServer(host, portnum)
 
     def say_hi(self):
-        print("hi there, everyone! my way or the highway.")
+        t = "hi there, everyone! my way or the highway."
+        self.sock.msend(t)
+        self.sock.mreceive()
+
+
 
 root = tk.Tk()
 root.title('Acme Chat')
 root.geometry("300x700")
 root.resizable(0, 0)
-root.configure(background='black')
+root.configure(background='blue')
 app = Application(master=root)
 app.mainloop()
